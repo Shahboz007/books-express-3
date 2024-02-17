50c97574d380d3640a6547821f8fcd22ebbe7330
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-02-17 16:42:43 +0500

    added page title

--- routes/books.js
+++ routes/books.js
@@ -7,8 +7,14 @@ const router = Router();
 // view path
 const viewsPath = path.join(__dirname, '..', 'views/books')
 
+// index page
 router.get('/', (req,res) => {
   res.sendFile(path.join(viewsPath, 'books.html'))
+})
+
+// add page
+router.get('/add', (req,res) => {
+  res.sendFile(path.join(viewsPath, 'books-add.html'))
 })
 
 module.exports = router;
